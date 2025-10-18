b862dd4786125fe747d3dabf04510f943e5b21dc
thapacodes4u <ankitgodarthapa@gmail.com>
2025-10-19 05:17:11 +0545

    fix: removing duplicate code

--- src/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ src/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -17,7 +17,9 @@ use LaraZeus\SpatieTranslatable\Resources\Pages\CreateRecord\Concerns\Translatab
 
 class CreateProduct extends CreateRecord
 {
-    use HandlesImageUploads;
+    use HandlesImageUploads {
+        afterCreate as handleImageUploadsAfterCreate;
+    }
     use HandlesTenantData, HasTenantFields;
     use Translatable;
 
@@ -93,59 +95,7 @@ class CreateProduct extends CreateRecord
             return;
         }
 
-        // Handle image uploads (from HandlesImageUploads trait logic)
-        $pendingImages = $this->temporaryImages;
-
-        if (! empty($pendingImages) && is_array($pendingImages)) {
-            foreach ($pendingImages as $index => $item) {
-                if (isset($item['temp_file'])) {
-                    $tempPath = storage_path('app/public/'.$item['temp_file']);
-
-                    if (file_exists($tempPath)) {
-                        $this->record->addMedia($tempPath)
-                            ->usingFileName($item['file_name'] ?? basename($tempPath))
-                            ->withCustomProperties([
-                                'name' => $item['name'] ?? [],
-                                'description' => $item['description'] ?? [],
-                                'is_cover' => $item['is_cover'] ?? false,
-                                'position' => $index,
-                            ])
-                            ->toMediaCollection('images');
-
-                        @unlink($tempPath);
-                    }
-                } elseif (isset($item['temp_url'])) {
-                    try {
-                        $this->record->addMediaFromUrl($item['temp_url'])
-                            ->usingFileName($item['file_name'] ?? basename($item['temp_url']))
-                            ->withCustomProperties([
-                                'name' => $item['name'] ?? [],
-                                'description' => $item['description'] ?? [],
-                                'is_cover' => $item['is_cover'] ?? false,
-                                'position' => $index,
-                            ])
-                            ->toMediaCollection('images');
-                    } catch (\Exception $e) {
-                    }
-                }
-            }
-
-            $coverMedia = $this->record->getMedia('images')
-                ->filter(fn ($media) => $media->getCustomProperty('is_cover', false));
-
-            if ($coverMedia->count() > 1) {
-                $coverMedia->skip(1)->each(function ($media) {
-                    $media->setCustomProperty('is_cover', false);
-                    $media->save();
-                });
-            }
-
-            if ($coverMedia->count() === 0 && $this->record->getMedia('images')->count() > 0) {
-                $firstMedia = $this->record->getMedia('images')->first();
-                $firstMedia->setCustomProperty('is_cover', true);
-                $firstMedia->save();
-            }
-        }
+        $this->handleImageUploadsAfterCreate();
 
         $state = $this->form->getState();
         $rawState = $this->form->getRawState();
